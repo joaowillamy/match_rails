568146c50c68f87955eafb34bf6f95572ca11b13
joaowillamy <willamyse@gmail.com>
2019-10-17 20:18:05 -0300

    feat(devise): configure permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_select, only: [:new, :edit], if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:brazilian_state_id, :email, :password]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
+
+  def configure_select
+    @brazilian_states_options = BrazilianState.all.pluck(:description, :id)
+  end
 end
